67cf7ae2d87e09367f7090309782ebef52b2f911
Mark <wangguohui@jingge.cc>
2015-01-21 23:05:19 +0800

    time

--- js/app/page.js
+++ js/app/page.js
@@ -1,7 +1,8 @@
 require.config({
     paths:{
 		"jquery":"../lib/jquery-1.9.0.min",
-		"slider":"../lib/jquery.superslide.2.1.1"
+		"slider":"../lib/jquery.superslide.2.1.1",
+		"times":"../module/time"
 	},
 	shim:{
 		slider:{
@@ -10,7 +11,7 @@ require.config({
 	}
 	
 });
-require(['jquery','slider'],function($,slider) {
+require(['jquery','slider','times'],function($,slider,times) {
 	$(function(){
 		
 		/* 销量排行 */
@@ -63,6 +64,39 @@ require(['jquery','slider'],function($,slider) {
 				plus.addClass("disabled");
 			}
 		});	
+		
+		//var at = new times();
+		//at.init();
+		
+		
+		function gt(){
+			var now = new Date();
+			var later = new Date("2/18/2015 13:00:00");//倒计时基准时间
+			var tim = (later.getTime() - now.getTime());
+			if(tim > 0){
+				days = tim / 1000 / 60 / 60 / 24;
+				daysRound = Math.floor(days);//剩余天
+				hours = tim / 1000 / 60 / 60 - (24 * daysRound);
+				hoursRound = Math.floor(hours);//剩余小时
+				minutes = tim / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound);
+				minutesRound = Math.floor(minutes);//剩余分钟
+				seconds = tim / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound);
+				secondsRound = Math.round(seconds);//剩余秒
+				
+				console.log(minutesRound);
+				
+				$("#saleDay").text(daysRound)
+				$("#saleHours").text(hoursRound)
+				$("#saleMinutes").text(minutesRound)
+				
+				setTimeout("gt()",1000)
+				
+			}else{
+				document.write('活动已经结束！');
+			};
+		};
+		
+		gt();
 		
 		
 	});
